84bae642ffb5bf6dd66d92a7543e886846c51b90
shot <wojciech.wolujewicz@protonmail.com>
2022-09-16 19:52:50 +0200

    covered some basics of working with static files

--- mysite/polls/tests/helper.py
+++ mysite/polls/tests/helper.py
@@ -9,7 +9,9 @@ def create_question(
     question_text, days, hours=0, minutes=0, seconds=0, choices=("choice 1", "choice 2")
 ):
     """
-    Create a question with the given 'question_text' and a set of 'choices' and published with an offset in 'days', either negative for past questions or positive for the future questions.
+    Create a question with the given 'question_text' and a set of 'choices'
+    and published with an offset in 'days', either negative for past questions
+    or positive for the future questions.
     """
     time = timezone.now() + datetime.timedelta(
         days=days, hours=hours, minutes=minutes, seconds=seconds
