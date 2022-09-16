e856f8a300cacd806118bc71c81a65d5e5fab11c
shot <wojciech.wolujewicz@protonmail.com>
2022-09-16 20:36:07 +0200

    Customized admin site

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    fieldsets = [
+        (None, {"fields": ["queston_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
